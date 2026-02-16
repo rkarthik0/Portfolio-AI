b99f1add3725195e2c4765c0067a91b4742bccfc
Karthik R <101082638+rkarthik0@users.noreply.github.com>
2026-02-16 16:44:38 +0530

    Update resume link text to 'My Resume'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ export default function Header() {
 
                 <a href="public/assets/KarthikR_scl.pdf" download
                     className="px-10 py-2.5 rounded-full border border-gray-300 dark:border-white/25 hover:bg-slate-100/70 dark:hover:bg-darkHover flex items-center gap-2 bg-white dark:bg-transparent dark:text-white">
-                    my resume <img src="./assets/download-icon.png" alt="" className="w-4 dark:invert" />
+                    My Resume <img src="./assets/download-icon.png" alt="" className="w-4 dark:invert" />
                 </a>
             </div>
         </div>
