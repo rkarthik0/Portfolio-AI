860ff01735bc0cc1bd42141ccba6c00bccc7fea2
Karthik R <101082638+rkarthik0@users.noreply.github.com>
2026-02-16 16:19:29 +0530

    Update resume link to point to new PDF location

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
                     contact me <img src="./assets/right-arrow-white.png" alt="" className="w-4" />
                 </a>
 
-                <a href="/assets/resumeee.pdf" download
+                <a href="public/assets/KarthikR_scl.pdf" download
                     className="px-10 py-2.5 rounded-full border border-gray-300 dark:border-white/25 hover:bg-slate-100/70 dark:hover:bg-darkHover flex items-center gap-2 bg-white dark:bg-transparent dark:text-white">
                     my resume <img src="./assets/download-icon.png" alt="" className="w-4 dark:invert" />
                 </a>
