f72ed1ff1479d7786163c9f1ae58cdc99d1a732b
Karthik R <101082638+rkarthik0@users.noreply.github.com>
2026-02-21 21:57:01 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
                     contact me <img src="./assets/right-arrow-white.png" alt="" className="w-4" />
                 </a>
 
-               <a href="public/assets/KarthikR.pdf" download
+               <a href="/assets/KarthikR.pdf" download
                     className="px-10 py-2.5 rounded-full border border-gray-300 dark:border-white/25 hover:bg-slate-100/70 dark:hover:bg-darkHover flex items-center gap-2 bg-white dark:bg-transparent dark:text-white">
                     My Resume <img src="./assets/download-icon.png" alt="" className="w-4 dark:invert" />
                 </a>
